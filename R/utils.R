d0b8026cc6d16159717f23a8a07a5ca8370e8ecd
Eric Lamphere <ericjlamphere@gmail.com>
2020-04-10 15:33:36 -0400

    see function

--- R/utils.R
+++ R/utils.R
@@ -6,6 +6,11 @@
 # FUNCTIONS ####
 ##########################################################-
 # Utility Functions ----
+see <- function(x){
+  View(x, title = deparse(substitute(x)))
+}
+
+# Pipe operators ----
 # x contains regex string
 `%~%` <- function(x, reg){
   grepl(reg, x, ignore.case = TRUE, perl = TRUE)
@@ -25,9 +30,21 @@
 }
 
 # x is not a value of y (list or vector)
-`!in` <- function(x, y){
+`%!in%` <- function(x, y){
   !(x %in% y)
 }
 
+# save environment
+saveEnv <- function(environment = .GlobalEnv){
+  .GlobalCopy <<- as.environment(as.list(environment, all.names = TRUE))
+}
 
 
+# restore saved environment
+# restoreEnv <- function(){
+#   # Remove difference between the saved copy and the current .GlobalEnv
+#   #set_env(.GlobalEnv, .GlobalCopy)
+#   rm(list = setdiff(ls(envir = .GlobalEnv, all.names = TRUE), '.GlobalCopy'))
+#   # assign the values from the saved copy back to the .GlobalEnv
+#   for(n in ls(.GlobalCopy, all.names = TRUE)) assign(n, get(n, .GlobalCopy), envir = .GlobalEnv)
+# }
